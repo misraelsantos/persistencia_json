06fa8a28e8b6d75db43bf781d419ca511eef910d
Misrael Santos <misraelsantos@gmail.com>
2021-07-27 22:20:14 -0300

    ler arquivo

--- src/main/java/util/LeitorJson.java
+++ src/main/java/util/LeitorJson.java
@@ -2,6 +2,8 @@ package util;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Reader;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,7 +17,7 @@ public class LeitorJson {
 	static Reader reader;
 	
 
-	public static void main(String[] args) throws FileNotFoundException {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		
 		reader = new FileReader("./src/main/resources/MassaTeste.json");
@@ -24,8 +26,18 @@ public class LeitorJson {
 		System.out.println(mapJson.get("url"));
 		System.out.println(mapJson.get("produto"));
 		System.out.println(mapJson.get("valor"));
+		gravaArquivoJson();
 
-
+	}
+	private static void gravaArquivoJson() throws IOException {
+		mapJson.put("url", "http://qaacademy.com.br");
+		mapJson.put("curso", "testes automatizados de api");
+		mapJson.put("endereço", "Av Paulista, 509");
+		
+		FileWriter writer = new FileWriter("./src/main/resources/massaTeste_writer.json");
+		gson.toJson(mapJson, writer); 
+		writer.flush();
+		
 	}
 
 }
